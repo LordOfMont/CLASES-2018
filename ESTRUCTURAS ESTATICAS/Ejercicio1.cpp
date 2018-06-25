85256588200f3b24645674fc206f453e1b7ce4e7
Lordofmont <cristian0106b@gmail.com>
2018-06-25 15:33:23 -0400

    Add files via upload

---
+++ ESTRUCTURAS ESTATICAS/Ejercicio1.cpp
@@ -0,0 +1,22 @@
+//ENTRADA
+//4
+//SALIDA
+//1 1 1 1 
+//2 2 2 2
+//3 3 3 3
+//4 4 4 4
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;//PEDIR el dato
+	int m[n][n];//CREO MI MATRIZ DE nxn
+	for(int i=0;i<n;i++){//CONTROLA LAS FILAS
+		for(int j=0;j<n;j++){//CONTROLA LAS COLUMNAS
+			m[i][j]=j+1;//HACE LA MAGIA 
+			cout<<m[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
+}
